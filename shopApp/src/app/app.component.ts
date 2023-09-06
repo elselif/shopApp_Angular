2f2695bacb88e408ff4056b4a0456d703ef68fba
Elif Emre <2003013054@student.beykent.edu.tr>
2023-09-06 14:11:22 +0300

    preparation of the project structure

---
+++ shopApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root',
+  template: `<div class="bg-primary m-5 p-5">
+  Shop Application
+  </div>`,
+})
+export class AppComponent {
+  title = '';
+}
